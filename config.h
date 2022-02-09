af497421ab1948091eadf83ac4b302e545ddf288
luis-casta7 <luis_casta7@outlook.com>
2022-02-09 00:48:15 -0600

    changes

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrainsMono-Regular:pixelsize=18:antialias=true:autohint=true";
+static char *font = "JetBrainsMono-Regular:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
